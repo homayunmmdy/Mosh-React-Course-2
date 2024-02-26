90cb3c792b353303da8df982548b6110fa612b0c
Homayoun <homayoun763@gmail.com>
2024-02-26 16:57:35 +0330

    Show battle in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,34 @@
-import Iframe from 'react-iframe'
+import  { useState, useEffect } from 'react';
+import axios from 'axios';
 
+interface BattleType {
+  _id: string;
+  name: string;
+}
 const App = () => {
+  const [battles, setBattles] = useState<BattleType[]>([]);
+
+  useEffect(() => {
+    fetchBattles();
+  }, []);
+
+  const fetchBattles = async () => {
+    try {
+      const response = await axios.get('/api/battles');
+      setBattles(response.data);
+    } catch (error) {
+      console.error('Error fetching battles:', error);
+    }
+  };
   return (
     <div>
-<Iframe url="https://www.linkedin.com/feed/"
-        width="640px"
-        height="320px"
-        id=""
-        className=""
-        display="block"
-        position="relative"/>    </div>
+      <h1>Battles</h1>
+      <ul>
+        {battles.map(battle => (
+          <li key={battle._id} className="py-2">{battle.name}</li>
+        ))}
+      </ul> 
+    </div>
   )
 }
 
