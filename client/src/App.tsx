0a713784aea32acc67b983eef7ed839735bcf0fb
Homayoun <homayoun763@gmail.com>
2024-02-26 16:25:56 +0330

    Add backend

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import Iframe from 'react-iframe'
+
+const App = () => {
+  return (
+    <div>
+<Iframe url="https://www.linkedin.com/feed/"
+        width="640px"
+        height="320px"
+        id=""
+        className=""
+        display="block"
+        position="relative"/>    </div>
+  )
+}
+
+export default App
