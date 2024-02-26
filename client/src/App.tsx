950eacf68133a2e53116dcc798602fbb6d2c68c4
Homayoun <homayoun763@gmail.com>
2024-02-27 00:00:21 +0330

    add admin route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Battle from "./pages/Battle/Battle";
+import Admin from "./pages/admin/Admin";
 
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
           </Routes>
           <Routes>
           <Route path="/battle/:id" element={<Battle />} />
+          </Routes>
+          <Routes>
+          <Route path="/admin" element={<Admin />} />
           </Routes>
       </Router>
     </>
