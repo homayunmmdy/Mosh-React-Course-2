6882ea06b1b412530ee8ce4df1a04a332e863003
Homayoun <homayoun763@gmail.com>
2024-05-23 14:24:20 +0330

    muted the data

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-interface Todo {
+export interface Todo {
   id: number;
   title: string;
   userId: number;
@@ -15,7 +15,7 @@ const useTodos = () => {
       .then((res) => res.data);
 
   return useQuery<Todo[], Error>({
-    queryKey: ["todo"],
+    queryKey: ["todos"],
     queryFn: fetchTodos,
     staleTime: 10 * 1000 //10s
   });
